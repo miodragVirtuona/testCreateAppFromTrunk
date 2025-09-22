76a79e5426d811a98d5e8d3150d8fa157a4575e8
tomcat <tomcat@CentOS-83-64-minimal>
2025-09-22 15:07:00 +0200

    test

---
+++ testapp13/trunk/default/build/clientcode/js/config.js
@@ -0,0 +1,14 @@
+var APP = {
+	FU_URI: "",
+	EXISTING_LANGUAGES : ["en"],
+	LANGUAGE_RESOURCES: {
+		"en": "http://dev.virtuona.rs/virtdev_scas/resources/testapp13AppType/en.json"
+	},
+	DEFAULT_LANGUAGE : "en",
+	APP_TYPE_PROJ : "http://dev.virtuona.rs/virtdev_scas/resources/TasorProjects/testapp13AppType",
+	APP_KB_PROJ : "http://dev.virtuona.rs/virtdev_scas/resources/TasorProjects/testapp13AppKB",
+	APP_ONTOS_PROJ : "http://dev.virtuona.rs/virtdev_scas/resources/TasorProjects/testapp13AppOntos",
+	APP_KB_WDS: "http://dev.virtuona.rs/virtdev_scas/resources/TasorProjects/testapp13AppKB_WDS",
+    APP_KB_DELETE_DS: "http://dev.virtuona.rs/virtdev_scas/resources/TasorProjects/testapp13AppKB_WDS_deletedResources",
+	EXT_LINKS : {},
+};
